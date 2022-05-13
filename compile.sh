08686a5462a0c3ca07c2967b18cebbae3abdda69
Alvaro Alonso <alonso.miguel.alvaro1@gmail.com>
2022-05-13 11:41:42 +0200

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to compile the java project with binaries and libraries
+# usage compile.sh <operating_system>
+if [ $# -ne 1 ]; then
+    echo "usage: compile.sh <operating_system>"
+    exit 1
+fi
+
+# Compile the project with windows
+if [ "$1" = "windows" ]; then
+    echo "Compiling for windows"
+    javac -d bin -cp bin;lib/jade.jar src/figbot/*.java
+    exit 0
+fi
+
+# Compile the project with linux
+if [ "$1" = "unix" ]; then
+    echo "Compiling for unix"
+    javac -d bin -cp bin:lib/jade.jar src/figbot/*.java
+    exit 0
+fi
---
+++ exec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to execute the java project with binaries and libraries
+# usage exec.sh <operating_system>
+if [ $# -ne 1 ]; then
+    echo "usage: exec.sh <operating_system>"
+    exit 1
+fi
+
+# Execute the project with windows
+if [ "$1" = "windows" ]; then
+    java -cp bin;lib/jade.jar figbot.Figbot
+    exit 0
+fi
+
+# Execute the project with unix
+if [ "$1" = "unix" ]; then
+    java -cp bin:lib/jade.jar figbot.Figbot
+    exit 0
+fi
